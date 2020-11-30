4e3cafb69b15f734f0ae6e9e9fdc9afe38030ffd
Murilo Beltrame <murilobeltrame@gmail.com>
2020-11-29 23:23:29 -0300

    Added Token Generation endpoint

---
+++ src/3.Framework/ApiShowcase.Auth/Controllers/TokenController.cs
@@ -0,0 +1,23 @@
+﻿using ApiShowcase.Auth.Models;
+using ApiShowcase.Auth.Repository;
+using ApiShowcase.Auth.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiShowcase.Auth.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TokenController : Controller
+    {
+        [HttpPost]
+        public IActionResult Authenticate([FromBody]User model)
+        {
+            var user = UserRepository.Get(model.Username, model.Password);
+            if (user == null) return NotFound(new { message = "Usuário ou senha inválidos" });
+            var token = TokenService.GenerateToken(user);
+            return Created("", new { token = token });
+        }
+    }
+}
