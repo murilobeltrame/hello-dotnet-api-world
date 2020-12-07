f1a37437b7903983a08b15accfb06a59c9c1f3ca
Murilo Beltrame <murilobeltrame@gmail.com>
2020-12-07 02:29:47 -0300

    Adicionando mapeamento do Banco de Dados

---
+++ src/3.Framework/ApiShowcase.Drivers.Data.SQLServer/Models/AwbuildVersion.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ApiShowcase.Drivers.Data.SQLServer.Models
+{
+    public partial class AwbuildVersion
+    {
+        public byte SystemInformationId { get; set; }
+        public string DatabaseVersion { get; set; }
+        public DateTime VersionDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
